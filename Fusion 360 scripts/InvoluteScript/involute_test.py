61899c957a050051adff66d235b81081ae75a122
Rishi <rishikalra001@gmail.com>
2020-05-17 11:50:46 +0100

    Updated gear tester to better investigate tooth meshing rotation

--- Fusion 360 scripts/InvoluteScript/involute_test.py
+++ Fusion 360 scripts/InvoluteScript/involute_test.py
@@ -4,68 +4,104 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 from involute import Involute
 
+class GearsPlotter():
+    def __init__(self, pressure_angle, pitch_diameter, module):
+        self._pressure_angle = pressure_angle
+        self._pitch_diameter = pitch_diameter
+        self._module = module
+        self.I = Involute(pressure_angle, pitch_diameter, 0.11)
+        self.fig = plt.figure(figsize = [8,8])
+        self.ax = ax = plt.axes([0.05, 0.05 + 0.1, 0.9, 0.8])
+        self.plot_func = self.plot_wheels_and_circles
+        self.ax.axis("equal")
+        self.ax.grid("on")
 
-pressure_angle = 20
-pitch_diameter = 20
-module = 0.1
+    @property
+    def pressure_angle(self):
+        return self._pressure_angle
 
-slider_height = 0.05
+    @pressure_angle.setter
+    def pressure_angle(self, value):
+        self._pressure_angle = value
+        self.re_plot()
+
+    @property
+    def pitch_diameter(self):
+        return self._pitch_diameter
+
+    @pitch_diameter.setter
+    def pitch_diameter(self, value):
+        self.pitch_diameter = value
+        self.re_plot()
+
+    @property
+    def module(self):
+        return self._module
+
+    @module.setter
+    def module(self, value):
+        self._module = value
+        self.re_plot()
+
+    def re_plot(self):
+        self.I = Involute(self.pressure_angle, self.pitch_diameter, self.module)
+        self.plot_func()
+
+    def plot_wheel(self):
+        self.ax.lines = []
+        x, y = self.I.create_wheel()
+        self.ax.plot(x,y, 'kx--')
+        self.plot_circle(self.I._pitch_radius, 'r--')
+        self.plot_circle(self.I._base_radius, 'r--')
+
+    def plot_single_tooth(self, angle):
+        self.ax.lines = []
+        x, y = I.create_single_tooth(angle)
+        self.ax.plot(x,y)
+        plot_circle(self.I._pitch_radius, 'r--')
+        plot_circle(self.I._base_radius, 'r--')
+
+    def plot_circle(self, radius, *args, **kwargs):
+        x,y = self.I.create_points_on_circle(radius)
+        self.ax.plot(x, y, *args, **kwargs)
+
+    def plot_wheels_and_circles(self):
+        self.ax.lines = []
+        x, y = self.I.create_wheel()
+        self.ax.plot(x + self.I._pitch_radius, y, 'k')
+        x, y = self.I.create_points_on_circle(self.I._pitch_radius)
+        self.ax.plot(x + self.I._pitch_radius, y, 'r--')
+        x, y = self.I.create_points_on_circle(self.I._base_radius)
+        self.ax.plot(x + self.I._pitch_radius, y, 'r--')
+
+        x, y = self.I.create_wheel()
+        self.ax.plot(x - self.I._pitch_radius, y, 'k')
+        x, y = self.I.create_points_on_circle(self.I._pitch_radius)
+        self.ax.plot(x - self.I._pitch_radius, y, 'r--')
+        x, y = self.I.create_points_on_circle(self.I._base_radius)
+        self.ax.plot(x - self.I._pitch_radius, y, 'r--')
+
+
+
+# create sliders
+props = {   "pressure_angle":{"initial": 20, "max":60, "min":1},
+            "pitch_diameter":{"initial": 10, "max":100, "min":0.1},
+            "module": {"initial": 0.1, "max":1, "min":0.001},
+        }
+
+GP = GearsPlotter(props["pressure_angle"]["initial"], props["pitch_diameter"]["initial"], props["module"]["initial"])
+GP.plot_wheels_and_circles()
+
+slider_width = 0.7
+slider_height = 0.025
 sliders = []
+slider_axs = []
 
-fig = plt.figure(figsize = [8,8])
-ax = plt.axes([0.05, 0.05 + 0.1, 0.9, 0.8])
-ax.axis("equal")
-ax.grid("on")
-
-I = Involute(pressure_angle, pitch_diameter, 0.11)
-
-def plot_wheel(module):
-    ax.lines = []
-    global pressure_angle, pitch_diameter, module
-    I = Involute(pressure_angle, pitch_diameter, module)
-    x, y = I.create_single_tooth(pi/4)
-    x, y = I.create_wheel()
-    ax.plot(x,y, 'kx--')
-    plot_circle(I._pitch_radius, 'r--')
-    plot_circle(I._base_radius, 'r--')
-
-def plot_single_tooth(angle):
-    ax.lines = []
-    x, y = I.create_single_tooth(angle)
-    ax.plot(x,y)
-    plot_circle(I._pitch_radius, 'r--')
-    plot_circle(I._base_radius, 'r--')
-
-def plot_circle(radius, *args, **kwargs):
-    global ax
-    x,y = I.create_points_on_circle(radius)
-    ax.plot(x, y, *args, **kwargs)
-
-def plot_wheels_and_circles(module):
-    global pressure_angle, pitch_diameter, module
-    ax.lines = []
-    I = Involute(pressure_angle, pitch_diameter, module)
-    x, y = I.create_wheel()
-    ax.plot(x + I._pitch_radius, y, 'k')
-    x, y = I.create_points_on_circle(I._pitch_radius)
-    ax.plot(x + I._pitch_radius, y, 'r--')
-    x, y = I.create_points_on_circle(I._base_radius)
-    ax.plot(x + I._pitch_radius, y, 'r--')
-
-    I = Involute(pressure_angle, pitch_diameter, module)
-    x, y = I.create_wheel()
-    ax.plot(x - I._pitch_radius, y, 'k')
-    x, y = I.create_points_on_circle(I._pitch_radius)
-    ax.plot(x - I._pitch_radius, y, 'r--')
-    x, y = I.create_points_on_circle(I._base_radius)
-    ax.plot(x - I._pitch_radius, y, 'r--')
-
-
-plot_wheels_and_circles(initial_module_value)
-
-ax2 = plt.axes([0.15, 0.05, 0.7, 0.05])
-S = Slider(ax2, "Module", 0.0, 1, valinit = initial_module_value)
-S.on_changed(plot_wheels_and_circles)
-
+for index, value in enumerate(props.keys()):
+    axs = plt.axes([0.15, 0.05 + index*slider_height, slider_width, slider_height])
+    S = Slider(axs, value, props[value]["min"], props[value]["max"], valinit = props[value]["initial"])
+    S.on_changed(lambda m: type(GP).__dict__[value].__set__(GP, m))
+    slider_axs.append(axs)
+    sliders.append(S)
 
 plt.show()
