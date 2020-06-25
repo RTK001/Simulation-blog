1eb359aefc5b511725f24df8c55594c0e5038f75
Rishi <rishikalra001@gmail.com>
2020-06-25 22:40:24 +0100

    Updated settings and added json files to send to heroku.

--- DjangoServer/SimulationBlog/SimulationBlog/settings.py
+++ DjangoServer/SimulationBlog/SimulationBlog/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANGO_SECRETKEY"]
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ["DJANGO_DEBUG"] == "TRUE")
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # configure django app for heroku
 import django_heroku
